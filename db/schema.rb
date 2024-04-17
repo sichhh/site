b8c7d6d694b96c792a4bc6b24de10dc778531834
Sichhh <mingaleev_emil@mail.ru>
2024-04-18 02:02:50 +0300

    Add styles and code for gems

--- db/schema.rb
+++ db/schema.rb
@@ -29,13 +29,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_13_201323) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.integer "age", null: false
-    t.string "last_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   add_foreign_key "comments", "articles"
 end
