d177f4d64711ad3950a0fbb38ae10cb5020287be
Sichhh <mingaleev_emil@mail.ru>
2024-10-09 18:17:53 +0300

    - Changed .rubocop.yml - Rolled out a new migration to remove reference - Fixed tests - Updated bin/quality - Switched to Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_413_201_323) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_07_165254) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.integer "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
-    t.index ["article_id"], name: "index_articles_on_article_id"
   end
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
-    t.integer "article_id", null: false
+    t.bigint "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
@@ -39,6 +40,5 @@ ActiveRecord::Schema[7.1].define(version: 20_240_413_201_323) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "articles", "articles"
   add_foreign_key "comments", "articles"
 end
