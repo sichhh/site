fda4f3d6388afb18db5513744fe0ac0cadabd1c8
Sichhh <mingaleev_emil@mail.ru>
2024-10-13 17:45:31 +0300

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 18 }
 end
