1ae8e688b518354648ba60148a0332ebbc1225f9
Sichhh <mingaleev_emil@mail.ru>
2024-10-14 11:15:20 +0300

    deleted the last migration

--- db/migrate/20241012083343_change_first_name_and_age_in_users.rb
+++
@@ -1,6 +0,0 @@
-class ChangeFirstNameAndAgeInUsers < ActiveRecord::Migration[7.1]
-  def change
-    change_column_null :users, :first_name, true
-    change_column_null :users, :age, true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_12_083343) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_10_152656) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,8 +33,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_12_083343) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.integer "age"
+    t.string "first_name", null: false
+    t.integer "age", null: false
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
