c17b861d28a1c2069feb476d60440f546e45705b
sichhh <77433697+sichhh@users.noreply.github.com>
2024-10-14 16:46:44 +0300

    Register (#5)
    * Register
    -Added the ability to login and register a user
    -Added option to log out
    
    * fix
    
    * fix
    
    * deleted the last migration
    
    * shema fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 18 }
 end
