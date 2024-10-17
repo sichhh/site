1db335ef5c5893cf9fdfac05f000d7324dd3e188
Sichhh <mingaleev_emil@mail.ru>
2024-10-17 15:52:38 +0300

    comment policy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_15_103928) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_16_154217) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,7 +31,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_15_103928) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.bigint "user_id", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -51,4 +53,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_15_103928) do
 
   add_foreign_key "articles", "users", on_delete: :cascade
   add_foreign_key "comments", "articles"
+  add_foreign_key "comments", "users", on_delete: :cascade
 end
