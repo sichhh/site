2c3d04ad08b3038ad9963c1a740d8e1cc5b25aa3
Sichhh <mingaleev_emil@mail.ru>
2024-10-20 12:12:56 +0300

    lest go

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,7 @@
 class Comment < ApplicationRecord
+  include Visible
   belongs_to :article
   belongs_to :user
 
   validates :body, presence: true
-  validates :status, inclusion: { in: %w[public private], message: "%<value>s is not a valid status" }
-
-  def archived?
-    status == "archived"
-  end
 end
