b9256ad55527bd2afb10f2e032714c834c374cc8
Sichhh <mingaleev_emil@mail.ru>
2024-10-21 18:31:32 +0300

    add policy test for edit, update, destroy

--- spec/policies/article_policy_spec.rb
+++ spec/policies/article_policy_spec.rb
@@ -1,16 +1,62 @@
 require "rails_helper"
+RSpec.describe ArticlePolicy do
+  let(:user) { create(:user) }
+  let(:article) { create(:article, user: user) }
+  let(:other_user) { create(:user) }
 
-RSpec.describe ArticlePolicyPolicy, type: :policy do
-  
-  describe_rule :index? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  let(:policy) { described_class.new(article, user: user) }
+
+  describe "#update?" do
+    subject { policy.apply(:update?) }
+
+    context "when the user is not the author" do
+      let(:policy) { described_class.new(article, user: other_user) }
+      it { is_expected.to eq false }
+    end
+
+    context "when the user is the author" do
+      it { is_expected.to eq true }
+    end
+
+    context "when the user is not authenticated" do
+      let(:policy) { described_class.new(article, user: nil) }
+      it { is_expected.to eq false }
+    end
   end
 
-  describe_rule :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+  describe "#edit?" do
+    subject { policy.apply(:edit?) }
 
-  describe_rule :manage? do
-    pending "add some examples to (or delete) #{__FILE__}"
+    context "when the user is not the author" do
+      let(:policy) { described_class.new(article, user: other_user) }
+      it { is_expected.to eq false }
+    end
+
+    context "when the user is the author" do
+      it { is_expected.to eq true }
+    end
+
+    context "when the user is not authenticated" do
+      let(:policy) { described_class.new(article, user: nil) }
+      it { is_expected.to eq false }
+    end
+
+    describe "#destroy?" do
+      subject { policy.apply(:destroy?) }
+
+      context "when the user is not the author" do
+        let(:policy) { described_class.new(article, user: other_user) }
+        it { is_expected.to eq false }
+      end
+
+      context "when the user is the author" do
+        it { is_expected.to eq true }
+      end
+
+      context "when the user is not authenticated" do
+        let(:policy) { described_class.new(article, user: nil) }
+        it { is_expected.to eq false }
+      end
+    end
   end
 end
