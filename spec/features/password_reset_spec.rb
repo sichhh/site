e3a36f6796bb608758335ad22a50f8585b6af0e5
Sichhh <mingaleev_emil@mail.ru>
2024-10-24 10:37:18 +0300

    password reset spec

---
+++ spec/features/password_reset_spec.rb
@@ -0,0 +1,16 @@
+RSpec.feature "Password Reset", type: :feature do
+  let(:user) { create(:user) }
+
+  scenario "User can reset their password" do
+    visit new_user_session_path
+
+    click_on "Forgot your password?"
+
+    fill_in "user[email]", with: user.email
+
+    click_on "Send me reset password instructions"
+
+    expect(page).to have_content("You will receive an email with instructions on how to reset your password in a few minutes.")
+
+  end
+end
