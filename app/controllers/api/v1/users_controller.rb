7787c53c6738c050a0a7f6ec4d329b3fdb05f226
sichhh <mingaleev_emil@mail.ru>
2024-12-16 12:22:32 +0300

    fix 2.0

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,24 +4,27 @@ module Api
       skip_before_action :verify_authenticity_token
 
       def show
-        @user = User.find(params[:id])
-        render json: @user
+        user = User.find(params[:id])
+
+        render json: UserSerializer.new(user).serializable_hash
       rescue ActiveRecord::RecordNotFound
         render json: { error: "User not found" }, status: :not_found
       end
 
       def create
-        @user = User.new(user_params)
-        if @user.save
-          render json: @user
+        user = User.new(user_params)
+
+        if user.save
+          render json: UserSerializer.new(user).serializable_hash, status: :created
         else
-          render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+          render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
         end
       end
 
       def upload_avatar
-        @user = User.find(params[:id])
-        if @user.update(avatar: params[:avatar])
+        user = User.find(params[:id])
+        
+        if user.update(avatar: params[:avatar])
           render json: { message: "Avatar uploaded successfully" }, status: :ok
         else
           render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
