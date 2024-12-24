c32a84cebe8a8ca07007e7cf5129550a889e9f12
sichhh <mingaleev_emil@mail.ru>
2024-12-24 15:21:59 +0300

    fix?

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 18 }
-  validates :avatar, attached: true, content_type: ["image/png", "image/jpg", "image/jpeg"]
+  validates :avatar, content_type: ["image/png", "image/jpg", "image/jpeg"]
 
   def avatar_thumbnail
     avatar.variant(resize_to_limit: [300, 300]).processed
