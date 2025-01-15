47a7ce03f6dcf6fa7604ac1647bf69631110c3f1
sichhh <mingaleev_emil@mail.ru>
2025-01-15 11:53:37 +0300

    find_spec fix

--- spec/interators/friendships/find_spec.rb
+++ spec/interators/friendships/find_spec.rb
@@ -1,26 +1,20 @@
 RSpec.describe Friendships::Find, type: :interactor do
   let(:friendship) { create :friendship }
-  let(:context) { { id: friendship.id } }
-
+  let(:friendship_id) { friendship.id }
+  let(:context) { { id: friendship_id } }
   describe ".call" do
     let(:call) { described_class.call(context) }
-
     context "when the friendship exists" do
       it "sets the friendship in the context" do
         expect(call[:friendship]).to eq(friendship)
       end
-
       it "does not fail" do
         call
         expect(call).to be_success
       end
     end
-
     context "when the friendship does not exist" do
-      before do
-        context[:id] = -1
-      end
-
+      let(:friendship_id) { nil }
       it "fails the context with an error" do
         call
         expect(call).to be_failure
