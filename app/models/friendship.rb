24b036fc58fcca2b26e89c685bbb62c057cffbc3
sichhh <mingaleev_emil@mail.ru>
2025-01-16 15:01:06 +0300

    add-email-noti

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -5,4 +5,12 @@ class Friendship < ApplicationRecord
   enum status: { pending: "pending", accepted: "accepted", rejected: "rejected" }
 
   validates :status, presence: true
+
+  after_create :send_friend_request_email
+
+  private
+
+  def send_friend_request_email
+    UserMailer.friend_request_email(self).deliver_later
+  end
 end
