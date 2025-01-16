5758b4f7704b9760f27eb591246d9c5c51b1dcdc
sichhh <mingaleev_emil@mail.ru>
2025-01-16 15:18:59 +0300

    shared_context fix

--- spec/support/shared_contexts/with_stubbed_active_record_order.rb
+++ spec/support/shared_contexts/with_stubbed_active_record_order.rb
@@ -1,7 +1,11 @@
-RSpec.shared_context "with stubbed active record order" do |options|
+shared_context "with stubbed active record order" do |options|
   before(:all) do
     options[:model].class_eval do
       default_scope { order(options[:order]) }
     end
+  end
+
+  after(:all) do
+    options[:model].default_scopes.pop
   end
 end
